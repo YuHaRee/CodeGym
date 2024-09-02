655344d1b7ab44e089ef3b682898a971b8969fa1
박찬호 <paekrng7@naver.com>
2024-09-02 20:57:19 +0900

    init: 초기 코드 테스트

---
+++ src/main/java/com/test/Solution.java
@@ -0,0 +1,16 @@
+package main.java.com.test;
+
+import java.util.Scanner;
+
+// 문자열 출력하기
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int a = str.length();
+
+        if(a >= 1 && a <= 1000000) {
+            System.out.println(str);
+        }
+    }
+}
