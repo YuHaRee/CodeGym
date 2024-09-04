90cc3120254d1ce4a465e4e178753fc0122d3c34
박찬호 <paekrng7@naver.com>
2024-09-04 20:32:04 +0900

    feature: a와 b 출력하기

--- src/main/java/com/test/Test01.java
+++ src/main/java/com/test/Test01.java
@@ -3,7 +3,7 @@ package main.java.com.test;
 import java.util.Scanner;
 
 // 문자열 출력하기.
-public class Solution {
+public class Test01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
